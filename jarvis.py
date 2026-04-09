8fdf9c4adc9642112b4c8a1276916ca2294a26f2
Pratik Patil <psp777323@gmail.com>
2026-04-09 14:15:38 +0530

    Correct speech recognition logic and variable names
    Fix recognition and variable assignment issues in jarvis.py.

--- jarvis.py
+++ jarvis.py
@@ -16,4 +16,14 @@ def take_command():
 
 with sr.Microphone() as source:
   print("Listening...")
-  r.pause
+  r.pause_threshold = 1
+  audio = r.listen(source)
+  try:
+    print("Recognizing..")
+    commabd = r.recognizing_google(audio, languag='en-in')
+    print("You said:", command)
+  except Exception:
+    speak("Sorry, I didn't understand.")
+    return "none"
+
+return commnd.lower()
